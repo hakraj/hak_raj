20ae1d9d9559b1cca0f20aae57c32eb03a8bbfbc
hakraj <adekunleddream@gmail.com>
2023-07-12 13:33:26 +0100

    change background gradient to lavender themed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         </Link>
 
 
-        <div className='space-x-8 sm:space-x-12'>
+        <div className='space-x-8 sm:space-x-12 text-slate-500 '>
           <Link className=' max-sm:hidden' href={"/"}>Home</Link>
           <Link href={"/"}>About</Link>
           <Link href={"/"}>Contact</Link>
@@ -25,7 +25,7 @@ export default function Home() {
       </div>
       <section className='z-10 w-full max-w-6xl pt-28 pb-20'>
         <div className='intro pt-[10vh] min-h-[30vh] '>
-          <p className='mb-4 font-light font-mono'>Hakeem Raji | Web developer</p>
+          <p className='mb-4 font-light font-mono text-slate-500 '>Hakeem Raji | Web developer</p>
           <h1 className='text-4xl leading-normal mb-4 font-medium'>I craft dynamic web app with seamless user experiences.</h1>
         </div>
       </section>
@@ -54,7 +54,7 @@ export default function Home() {
       </div> */}
 
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+      {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
@@ -63,7 +63,7 @@ export default function Home() {
           height={37}
           priority
         />
-      </div>
+      </div> */}
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
